f2dd8470e8d1aa8e5710d969272d752f3e4c33af
Egor Melnikov <pc.player@mail.ru>
2024-08-22 22:30:56 +0300

    Deck

--- Card_Game.py
+++ Card_Game.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 class Card:
     suit = ["Пикей",
             "Червей",
@@ -37,3 +39,16 @@ class Card:
     def __repr__(self):
         v = self.values[self.value] + " of " + self.suits[self.suit]
         return v
+    
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards.append(Card(i, j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
