7848d43843618c03e38cfad058fd4cfc1eb85f71
Mathitiss <pc.player@mail.ru>
2025-07-22 21:30:58 +0300

    nums_check

--- Bagels.py
+++ Bagels.py
@@ -1,3 +1,6 @@
+import random
+import re
+
 print('''
     Bagels, a deductive logic game.
 
@@ -21,9 +24,76 @@ print('''
     Print - 7 (GOD)
       ''')
 
-difficult = int(input('---> '))
-difs = {1: 100_000, 2: 100, 3: 50, 4: 25, 5: 10, 6: 3, 7: 1}
-if difficult == 1:
-    print('You have UNLIMITED guesses to get it.')
-else:
-    print(f'You have {difs[difficult]} guesses to get it.')
+class Game:
+    diffs = {1: 100_000, 2: 100, 3: 50, 4: 25, 5: 10, 6: 3, 7: 1}
+
+    def __init__(self, num):
+        self.difficult = self.diffs[num]
+        self.result = self.random_number()
+
+        if self.difficult == '1':
+            print('You have UNLIMITED guesses to get it.')
+            print()
+        else:
+            print(f'You have {self.difficult} guesses to get it. Good luck!')
+            print()
+
+        self.play()
+
+    def random_number(self):
+        return str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+    
+    def check(self):
+        flag = True
+
+        while flag:
+            word = input('Your guess --> ')
+
+            if not re.match('\d{3}', word):
+                print('''ERROR! Your number should consist of 3 digits. Examples: 123, 100, 023, 003, 000, etc.
+    Try again!
+                    ''')
+            else:
+                flag = False
+
+        return word
+
+    def play(self):
+        if self.difficult != 1:
+            while self.difficult:
+                guess = self.check()
+
+                if guess == self.result:
+                    print('CORRECT! YOU WIN')
+                    return
+                else:
+                    print('WRONG. TRY AGAIN')
+                    print()
+
+                self.difficult -= 1
+        else:
+            guess = self.check()
+
+            if guess == self.result:
+                print('CORRECT! YOU WIN')
+                return
+
+        print('YOU LOST! GAME OVER')
+        return
+    
+def level():
+    flag = True
+        
+    while flag:
+        n = input('Choose difficulty ---> ')
+    
+        if n.isdigit() and int(n) in list(range(1, 8)):
+            flag = False
+        else:
+            print('''ERROR! Difficulty level should be a digit in range 1 to 7.
+Try again!
+                    ''')
+    return int(n)
+
+l = level()
+Game(l)
